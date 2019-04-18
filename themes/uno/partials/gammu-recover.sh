e7d75f80ddbb5f3547ca3e9d30fa8bfca74fa424
Loc Nguyen <me@locnh.com>
2019-04-19 02:13:55 +0700

    Fuck Hubpress

---
+++ themes/uno/partials/gammu-recover.sh
@@ -0,0 +1,95 @@
+➜  partials git:(master) ✗ git status
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git checkout -- <file>..." to discard changes in working directory)
+
+        modified:   ../../../hubpress/config.json
+        modified:   social.hbs
+
+no changes added to commit (use "git add" and/or "git commit -a")
+remote: Compressing objects: 100% (18/18), done.
+remote: Total 24 (delta 14), reused 0 (delta 0), pack-reused 0
+Unpacking objects: 100% (24/24), done.
+From github.com:locnh/locnh.github.io
+   c8387ac..daae45f  master     -> origin/master
+Updating c8387ac..daae45f
+Fast-forward
+ 2016/06/29/Docker-for-Magento-how-to-prepare-develop-environment-in-10-senconds.html | 4 ++--
+ 2016/07/16/Best-practices-for-setting-up-Docker-on-Ubuntu-Dev-machine.html           | 4 ++--
+ hubpress/config.json                                                                 | 9 ++++-----
+ index.html                                                                           | 4 ++--
+ rss/index.xml                                                                        | 2 +-
+ tag/Dev-Ops/index.html                                                               | 4 ++--
+ tag/Docker/index.html                                                                | 4 ++--
+remote: Compressing objects: 100% (18/18), done.
+remote: Total 24 (delta 14), reused 0 (delta 0), pack-reused 0
+Unpacking objects: 100% (24/24), done.
+From github.com:locnh/locnh.github.io
+   08c6a14..b53c218  master     -> origin/master
+Updating 08c6a14..b53c218
+Fast-forward
+ 2016/06/29/Docker-for-Magento-how-to-prepare-develop-environment-in-10-senconds.html | 11 +++--------
+ 2016/07/16/Best-practices-for-setting-up-Docker-on-Ubuntu-Dev-machine.html           | 11 +++--------
+ hubpress/config.json                                                                 | 12 ++++++++++--
+ index.html                                                                           | 11 +++--------
+ rss/index.xml                                                                        |  2 +-
+ tag/Dev-Ops/index.html                                                               | 11 +++--------
+ tag/Docker/index.html                                                                | 11 +++--------
+ tag/Magento/index.html                                                               | 11 +++--------
+ 8 files changed, 29 insertions(+), 51 deletions(-)
+➜  locnh.github.io git:(master) ✗ git status
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git checkout -- <file>..." to discard changes in working directory)
+
+        modified:   themes/uno/partials/social.hbs
+
+no changes added to commit (use "git add" and/or "git commit -a")
+➜  locnh.github.io git:(master) ✗ git pull
+Already up to date.
+➜  locnh.github.io git:(master) ✗
+➜  locnh.github.io git:(master) ✗ git pull
+Already up to date.
+➜  locnh.github.io git:(master) ✗ git add .
+➜  locnh.github.io git:(master) ✗ git commit -m "Fuck Hubpress"
+[master f9a89c7] Fuck Hubpress
+ 2 files changed, 5 insertions(+), 7 deletions(-)
+➜  locnh.github.io git:(master) git push
+Enumerating objects: 13, done.
+Counting objects: 100% (13/13), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (7/7), done.
+Writing objects: 100% (7/7), 710 bytes | 710.00 KiB/s, done.
+Total 7 (delta 6), reused 0 (delta 0)
+remote: Resolving deltas: 100% (6/6), completed with 6 local objects.
+To github.com:locnh/locnh.github.io.git
+   b53c218..f9a89c7  master -> master
+➜  locnh.github.io git:(master)
+➜  locnh.github.io git:(master) ✗ git add .
+➜  locnh.github.io git:(master) ✗ git commit -m "Fuck Hubpress"
+[master fda3417] Fuck Hubpress
+ 2 files changed, 2 insertions(+), 2 deletions(-)
+➜  locnh.github.io git:(master) git push
+Merge branch 'master' of github.com:locnh/locnh.github.io
+
+# Please enter a commit message to explain why this merge is necessary,
+# especially if it merges an updated upstream into a topic branch.
+#
+# Lines starting with '#' will be ignored, and an empty message aborts
+# the commit.
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"~/WorkShop/locnh.github.io/.git/MERGE_MSG" 7L, 285C
